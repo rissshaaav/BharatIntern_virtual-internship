e5f128d6d6ffef9a90dafc30c7abd8e7a6d7a71e
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-01-18 02:25:03 +0530

    upload working correctly

--- task2_blogs/bee_blogs/src/components/Editor/Main.js
+++ task2_blogs/bee_blogs/src/components/Editor/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { storage } from "../../utils/Firebase";
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import Banner from "./Banner";
@@ -11,50 +11,8 @@ const Main = () => {
   const [content, setContent] = useState("");
   const [keywords, setKeywords] = useState([]);
 
-  // const handleUpload = () => {
-  //   if (!banner) {
-  //     return alert("No file selected");
-  //   }
-  //   const storageRef = ref(storage, `images/${banner.name}`);
-  //   const uploadTask = uploadBytesResumable(storageRef, banner);
-  //   uploadTask.on(
-  //     "state_changed",
-  //     (snapshot) => {
-  //       const progress =
-  //         (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-  //       console.log("Upload is " + progress + "% done");
-  //       switch (snapshot.state) {
-  //         case "paused":
-  //           console.log("Upload is paused");
-  //           break;
-  //         default:
-  //           console.log("Upload is running");
-  //       }
-  //     },
-  //     (error) => {
-  //       switch (error.code) {
-  //         case "storage/unauthorized":
-  //           console.log("User doesn't have permission to access the object");
-  //           break;
-  //         case "storage/canceled":
-  //           console.log("User canceled the upload");
-  //           break;
-  //         case "storage/unknown":
-  //           console.log("Unknown error occurred, inspect error.serverResponse");
-  //           break;
-  //         default:
-  //           console.log("uploaded successfully");
-  //       }
-  //     },
-  //     () =>
-  //       getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-  //         setDownloadURL(url);
-  //       })
-  //   );
-  // };
   const handleSubmit = async () => {
     try {
-      // Upload banner to Firebase Storage
       const storageRef = ref(storage, `images/banner/${banner.name}`);
       const uploadTask = uploadBytesResumable(storageRef, banner);
       uploadTask.on("state_changed", null, null,
@@ -62,31 +20,39 @@ const Main = () => {
         const url =  await getDownloadURL(uploadTask.snapshot.ref);
         setBannerURL(url);
       });
-      // const bannerSnapshot = await storageRef.put(banner);
-      // const bannerURL = await bannerSnapshot.ref.getDownloadURL();
-
-      // Make fetch request to store data in MongoDB
-      const res = await fetch("http://localhost:3001/write", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          banner: bannerURL,
-          blog: content,
-          keywords: keywords,
-        }),
-      });
-
-      if (res.ok) {
-        setContent("");
-        setKeywords([]);
-      }
-      console.log(await res.json());
     } catch (error) {
       console.log(error);
     }
   };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/write", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            banner: bannerURL,
+            blog: content,
+            keywords: keywords,
+          }),
+        });
+
+        // if (res.ok) {
+        //   setContent(""); //this and
+        //   setKeywords([]); //this was causing infinite rendering
+        // }
+        console.log(await res.json());
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (bannerURL !== "") {
+      fetchData();
+    }
+  }, [bannerURL, content, keywords]);
   return (
     <div>
       <Banner setBanner={setBanner} />
